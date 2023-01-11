960dd20c45f1c711be645c15a35da0220e0d652e
Gaurav <gauravnit006@gmail.com>
2023-01-11 14:41:02 +0530

    first

---
+++ tut7.py
@@ -0,0 +1,12 @@
+# Comments, Escape Sequences & Print Statement
+"""
+for
+multi
+line
+comment
+"""
+"""This is a comment"""
+# print("This is a laptop.", end=" ")
+# print("And it is used for education.")
+# print("This is a laptop.","And it is used for education.")
+print("C:\"narry\"")
