bb05253568a20a2b31ef33adeb06987df5099a78
Gaurav <gauravnit006@gmail.com>
2023-01-12 18:00:54 +0530

    Push tut10

---
+++ tut10.py
@@ -0,0 +1,12 @@
+# product = ['Vim bar', "Detergent", "Potato", 'Tomato', 210]
+# product.sort()
+# print(product)
+
+numbers = [43, 12, 4, 20, 14]
+# numbers.sort()
+# numbers.reverse()
+# print(numbers[::-1])
+# print(len(numbers))
+# print(max(numbers))
+numbers.append((75))
+print(numbers)
